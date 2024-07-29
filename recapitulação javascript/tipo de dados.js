97726cfcd15d507b93c2f576e1eb665b2a094b62
HDS0219 <dan1902heller@gmail.com>
2024-07-29 17:01:25 -0300

    first commit

---
+++ recapitulação javascript/tipo de dados.js
@@ -0,0 +1,28 @@
+var nome = "João";
+let idade = 25;
+const PI = 3.14;
+
+console.log("NOME: " + nome + "IDADE: " + idade + "\n o valor de pi é: " + PI)
+
+//tipos de dados
+/*
+
+let nome = "Ana"; // string
+let idade = 30; // number
+let casado = false; // boolean
+let vazio = null; // null
+let indefinido; // undefined
+let pessoa = { nome: "Carlos", idade: 27 }; // object
+let lista = [1, 2, 3]; // array
+
+*/
+
+/* operadores
+
+let a = 5, b = 10;
+let soma = a + b; // 15
+let ehMaior = a > b; // false
+let eVerdade = (a < b) && (b > 0); // true
+
+*/
+
